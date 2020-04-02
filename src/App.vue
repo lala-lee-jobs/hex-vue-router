e9c4c1d6729ae168301d982e243f17830c7b274e
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-02 13:09:02 +0800

    新增路由路徑及連結,router-link使用to="path",path是route的path屬性

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="#">Home</a> -->
+            <router-link class="nav-link" to="/index">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="#">Page</a> -->
+            <router-link class="nav-link" to="/page">Page</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>      
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
@@ -20,32 +35,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '../components/HelloWorld';
+import Page from '../components/pages/page';
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,11 @@ export default new VueRouter( {
             name: '首頁',
             path: '/index',
             component: Home,
+        },
+        {
+            name: '分頁',
+            path: '/page',
+            component: Page,
         }
     ]
 });
